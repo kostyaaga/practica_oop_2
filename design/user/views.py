fd344fcaa2f6ca0cdfb26e90cf7ffc9e9dbc7a85
aga <146637209+kostyaaga@users.noreply.github.com>
2024-10-24 23:28:07 +0700

    login+regitrate

---
+++ design/user/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib import messages
+
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                form.add_error(None, 'Неверное имя пользователя или пароль')
+    else:
+        form = LoginForm()
+
+    return render(request, 'user/login.html', {'form': form})
+
+def user_logout(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('index')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            messages.success(request, 'Вы успешно зарегистрированы! Теперь вы можете войти.')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'user/register.html', {'form': form})
