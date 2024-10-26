102a51449f5ebfd675aa29f20d162161e2792a40
aga <146637209+kostyaaga@users.noreply.github.com>
2024-10-26 17:07:15 +0700

    change_status

---
+++ design/user/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from .views import user_login, user_logout, register, profile, create_request, delete_request, update_request_status
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = ([
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    path('create-request/', create_request, name='create_request'),
+    path('delete_request/<int:request_id>/', delete_request, name='delete_request'),
+    path('update_request_status/<int:request_id>/', update_request_status, name='update_request_status'),
+]
+               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
